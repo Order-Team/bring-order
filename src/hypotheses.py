1e90ef2f6106b047a175b2c5f302fc346e316f27
Sini Arkko <83418050+SiniCode@users.noreply.github.com>
2023-05-19 12:33:17 +0300

    Fix grammar

--- src/hypotheses.py
+++ src/hypotheses.py
@@ -4,24 +4,24 @@ from IPython.display import display
 class Hypotheses:
     def __init__(self):
         style = {'description_width':'initial'}
-        self.hypotheses = widgets.Text(value='', description='Hypotheses:', style=style)
-        self.null_hypotheses = widgets.Text(value='', description='Null hypotheses:', style=style)
+        self.hypothesis = widgets.Text(value='', description='Hypothesis:', style=style)
+        self.null_hypothesis = widgets.Text(value='', description='Null hypothesis:', style=style)
         save_button, clear_button = self._initialize_buttons()
         self.view1 = widgets.VBox(
-            [self.hypotheses,
-             self.null_hypotheses,
+            [self.hypothesis,
+             self.null_hypothesis,
              widgets.HBox([save_button, clear_button])])
 
     def _clear_button_clicked(self, _):
-        self.hypotheses.value = ''
-        self.null_hypotheses.value = ''
+        self.hypothesis.value = ''
+        self.null_hypothesis.value = ''
 
     def _save_button_clicked(self, _):
-        self.hypotheses = f'Hypotheses: {self.hypotheses.value}'
-        self.null_hypotheses = f'Null hypotheses: {self.null_hypotheses.value}'
+        self.hypothesis = f'Hypothesis: {self.hypothesis.value}'
+        self.null_hypothesis = f'Null hypothesis: {self.null_hypothesis.value}'
         self.view1.close()
-        print(self.hypotheses)
-        print(self.null_hypotheses)
+        print(self.hypothesis)
+        print(self.null_hypothesis)
 
     def _initialize_buttons(self):
         save_button = widgets.Button(description='Save')
