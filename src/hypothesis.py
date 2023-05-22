67c47435869641b985b9d08577c5cfc1f5459ca3
aleksinuora <aleksinuora@gmail.com>
2023-05-22 22:11:24 +0300

    grid layout and button styles

--- src/hypothesis.py
+++ src/hypothesis.py
@@ -5,14 +5,21 @@ class Hypothesis:
     """_summary_
     """
     def __init__(self):
-        style = {'description_width':'initial'}
-        self.hypothesis = widgets.Text(value='', description='Hypothesis:', style=style)
-        self.null_hypothesis = widgets.Text(value='', description='Null hypothesis:', style=style)
+        self.hypothesis_label = widgets.Label(value='Hypothesis:',
+                                              layout=widgets.Layout(justify_content='flex-end'))
+        self.null_hypothesis_label = widgets.Label(value='Null hypothesis:',
+                                                layout=widgets.Layout(justify_content='flex-end'))
+        self.hypothesis = widgets.Text(value='')
+        self.null_hypothesis = widgets.Text(value='')
         save_button, clear_button = self.initialize_buttons()
-        self.view1 = widgets.VBox(
-            [self.hypothesis,
-             self.null_hypothesis,
-             widgets.HBox([save_button, clear_button])])
+        grid = widgets.GridspecLayout(3, 3, height='auto')
+        grid[0, 0] = self.hypothesis_label
+        grid[0, 1] = self.hypothesis
+        grid[1, 0] = self.null_hypothesis_label
+        grid[1, 1] = self.null_hypothesis
+        grid[2, 1] = widgets.HBox([save_button, clear_button])
+        self.view1 = grid
+
 
     def clear_button_clicked(self, _=None):
         """_summary_
@@ -41,8 +48,8 @@ class Hypothesis:
         Returns:
             _type_: _description_
         """
-        save_button = widgets.Button(description='Save')
-        clear_button = widgets.Button(description='Clear')
+        save_button = widgets.Button(description='Save', button_style='success')
+        clear_button = widgets.Button(description='Clear', button_style='info')
         save_button.on_click(self.save_button_clicked)
         clear_button.on_click(self.clear_button_clicked)
         return save_button, clear_button
