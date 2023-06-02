d8f1306299a83e7a34c9fcd8b059da12c2b6c4e5
Sini Arkko <83418050+SiniCode@users.noreply.github.com>
2023-06-02 14:32:21 +0300

    Update main class

--- src/bringorder.py
+++ src/bringorder.py
@@ -1,11 +1,25 @@
 import os
 import sys
+wd = os.getcwd()
 class_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(1, class_dir)
 
-from bogui import BOGui
+from analysis import Analysis
 
-class BringOrder():
+
+class BringOrder:
+    """Main class"""
     def __init__(self):
-        self.bogui = BOGui()
-        self.bogui()
+        """Class constructor"""
+        self.analyses = []
+
+    def new_analysis(self, start_cell):
+        """Starts new analysis.
+
+        Args:
+            start_cell (int): the index of the notebook cell where the method is called
+        """
+        current_cell = start_cell
+        new_analysis = Analysis(current_cell)
+        self.analyses.append(new_analysis)
+        new_analysis.start_analysis()
