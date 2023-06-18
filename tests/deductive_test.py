f7af1630ab1e2b1ce84d57959448cd04acb1a265
Taria Solasuo <taria.solasuo@helsinki.fi>
2023-06-18 14:53:30 +0300

    Deductive unit and robot tests updated

--- tests/deductive_test.py
+++ tests/deductive_test.py
@@ -27,30 +27,30 @@ class TestDeductive(unittest.TestCase):
         self.assertEqual(self.instance.empty_hypo_error.value, 'Hypothesis missing')
         self.assertEqual(self.instance.empty_null_error.value, 'Null hypothesis missing')
 
-    # def test_valid_inputs_are_accepted(self):
-    #     self.instance.hypothesis_input.value = "The Earth is flat"
-    #     self.instance.null_input.value = "The Earth is round"
-    #     testValue = self.instance.check_hypotheses()
-    #     self.assertEqual(self.instance.empty_hypo_error.value, '')
-    #     self.assertEqual(self.instance.empty_null_error.value, '')
-    #     self.assertTrue(testValue)
+    def test_valid_inputs_are_accepted(self):
+        self.instance.hypotheses[0].value = "The Earth is flat"    
+        self.instance.hypotheses[1].value = "The Earth is round"
+        testValue = self.instance.check_hypotheses()
+        self.assertEqual(self.instance.empty_hypo_error.value, '')
+        self.assertEqual(self.instance.empty_null_error.value, '')
+        self.assertTrue(testValue)
+    
+    def test_hypothesis_fields_can_be_cleared(self):
+        self.instance.hypotheses[0].value = "Roses are red"
+        self.instance.hypotheses[1].value = "Violets are blue"
+        self.instance.clear_hypotheses()
+        self.assertEqual(self.instance.hypotheses[0].value, '')
+        self.assertEqual(self.instance.hypotheses[1].value, '')
 
-    # def test_hypothesis_fields_can_be_cleared(self):
-    #     self.instance.hypothesis_input.value = "Roses are red"
-    #     self.instance.null_input.value = "Violets are blue"
-    #     self.instance.clear_hypotheses()
-    #     self.assertEqual(self.instance.hypothesis_input.value, '')
-    #     self.assertEqual(self.instance.null_input.value, '')
-
-    # def test_saving_hypotheses_displays_grid(self):
-    #     self.instance.hypothesis_input.value = "Roses are red"
-    #     self.instance.null_input.value = "Violets are blue"
-    #     Deductive.check_hypotheses = MagicMock()
-    #     Deductive.check_hypotheses.return_value = True
-    #     Deductive.create_confirmed_grid = MagicMock()
-    #     Deductive.create_confirmed_grid.return_value = "Hello"
-    #     self.instance.save_hypotheses(self)
-    #     self.assertIsNotNone(self.instance.confirmed_grid)
+    def test_saving_hypotheses_displays_grid(self):
+        self.instance.hypotheses[0].value = "Roses are red"
+        self.instance.hypotheses[1].value = "Violets are blue"
+        Deductive.check_hypotheses = MagicMock()   
+        Deductive.check_hypotheses.return_value = True
+        Deductive.create_confirmed_grid = MagicMock()
+        Deductive.create_confirmed_grid.return_value = "Hello"
+        self.instance.save_hypotheses(self)
+        self.assertIsNotNone(self.instance.confirmed_grid)
 
     def test_bad_hypotheses_are_cleared(self):
         self.instance.clear_hypotheses = MagicMock()
