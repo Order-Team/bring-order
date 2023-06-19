edd92aa6e849f016a8b1c3e3873a930d862a5660
Sini Arkko <83418050+SiniCode@users.noreply.github.com>
2023-06-19 13:43:22 +0300

    Update bodi tests

--- tests/bodi_test.py
+++ tests/bodi_test.py
@@ -16,6 +16,9 @@ class TestBodi(unittest.TestCase):
         self.instance.boutils = Mock()
         self.instance.bogui = Mock()
 
+    def test_correct_amount_of_buttons_is_created(self):
+        self.assertEqual(len(self.instance.buttons), 6)
+
     def test_start_data_hides_current_input(self):
         self.instance.data_description.value = 'Some description'
         self.instance.start_data_import()
@@ -28,18 +31,43 @@ class TestBodi(unittest.TestCase):
         value = "Some limitations"
         self.assertTrue(self.instance.check_limitations(value))
 
-    def test_open_cells_button_creates_button(self):
-        self.instance.create_open_cells_button()
-        self.instance.bogui.create_button.assert_called()
+    def test_open_cells_increases_cell_count_with_default_one(self):
+        correct_cell_count = self.instance.cell_count + 1
+        self.instance.open_cells()
+        self.assertEqual(self.instance.cell_count, correct_cell_count)
 
-    def test_create_delete_button_creates_button(self):
-        self.instance.create_delete_button()
-        self.instance.bogui.create_button.assert_called()
+    def test_open_cells_increases_cell_count_with_selected_value(self):
+        self.instance.add_cells_int.value = 3
+        correct_cell_count = self.instance.cell_count + 3
+        self.instance.open_cells()
+        self.assertEqual(self.instance.cell_count, correct_cell_count)
 
-    def test_create_run_button_creates_button(self):
-        self.instance.create_run_button()
-        self.instance.bogui.create_button.assert_called()
+    def test_open_cells_creates_code_cell(self):
+        self.instance.open_cells()
+        self.instance.boutils.create_code_cells_above.assert_called_with(1)
 
-    def test_create_analysis_button_creates_button(self):
-        self.instance.create_analysis_button()
-        self.instance.bogui.create_button.assert_called()
+    def test_open_cells_creates_selected_number_of_code_cells(self):
+        self.instance.add_cells_int.value = 3
+        self.instance.open_cells()
+        self.instance.boutils.create_code_cells_above.assert_called_with(3)
+
+    def test_delete_last_cell_does_not_delete_extra_cells(self):
+        correct_cell_count = self.instance.cell_count
+        self.instance.delete_last_cell()
+        self.assertEqual(self.instance.cell_count, correct_cell_count)
+        self.instance.boutils.delete_cell_above.assert_not_called()
+
+    def test_delete_last_cell_decreases_cell_count(self):
+        self.instance.cell_count = 3
+        self.instance.delete_last_cell()
+        self.assertEqual(self.instance.cell_count, 2)
+
+    def test_delete_last_cell_removes_cell(self):
+        self.instance.open_cells()
+        self.instance.delete_last_cell()
+        self.instance.boutils.delete_cell_above.asset_called()
+
+    def test_start_analysis_clicked_checks_limitations(self):
+        self.instance.call_check_limitation = Mock()
+        self.instance.start_analysis_clicked()
+        self.instance.call_check_limitation.assert_called()
