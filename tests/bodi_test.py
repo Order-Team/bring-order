d46e8a0e219a5c80bfce91cfb82efd38131281ca
aleksinuora <aleksinuora@gmail.com>
2023-08-02 11:50:56 +0300

    Updated tests

--- tests/bodi_test.py
+++ tests/bodi_test.py
@@ -20,7 +20,7 @@ class TestBodi(unittest.TestCase):
         self.instance.limitations.empty_limitations_error = self.instance.bogui.create_error_message('')
 
     def test_correct_amount_of_buttons_is_created(self):
-        self.assertEqual(len(self.instance.buttons), 10)
+        self.assertEqual(len(self.instance.buttons), 11)
 
     def test_bodi_hides_current_input(self):
         self.instance.bodi()
