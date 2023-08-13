c9890e2eda0228dfae48ce44848026c2592f684c
Tino Pönni <tino.ponni@tutanota.com>
2023-08-13 21:43:51 +0300

    restore bodi

--- bring_order/bodi.py
+++ bring_order/bodi.py
@@ -9,20 +9,33 @@ class Bodi:
     """Creates code cells for importing data and markdown cell(s) to describe data limitations"""
     def __init__(self, boutils, bogui, next_step):
         """Class constructor
+            Args:
+                bogui: Bring Order GUI components class
+                boutils: Bring order helper utilities class
+                next_step: next action to do (str)
+
+            fields: text and integer fiels
+                    0 = title
+                    1 = data_name
+                    2 = data_description
+                    3 = add_cells_int
         """
         self.boutils = boutils
         self.bogui = bogui
         self.cell_count = 0
         self.buttons = self.bogui.init_buttons(self.button_list)
-        self.title = self.bogui.create_input_field()
-        self.data_name = self.bogui.create_input_field()
-        self.data_description = self.bogui.create_text_area()
-        self.add_cells_int = self.bogui.create_int_text()
+        self.fields = [self.bogui.create_input_field(),
+                       self.bogui.create_input_field(),
+                       self.bogui.create_text_area(),
+                       self.bogui.create_int_text()]
+        #self.title = self.bogui.create_input_field()
+        #self.data_name = self.bogui.create_input_field()
+        #self.data_description = self.bogui.create_text_area()
+        #self.add_cells_int = self.bogui.create_int_text()
         self.limitations = Limitations(self.bogui)
         self.file_chooser = self.bogui.create_file_chooser()
         self.stattests = Stattests(self.bogui)
         self.next_step = next_step
-
 
     @property
     def button_list(self):
@@ -32,24 +45,26 @@ class Bodi:
             list of tuples in format (tag: str, description: str, command: func, style: str)
         """
         button_list = [
-            ('save', 'Save description', self.start_data_import, 'success'),
-            ('analyze', 'Analyze this data', self.check_variables, 'success'),
-            ('test', 'Test', self.check_variable_independence, 'success'),
-            ('close', 'Close test', self.close_independence_test, 'warning'),
-            ('independence', 'Test independence', self.display_independence_test, 'success'),
-            ('import', 'Import manually', self.show_cell_operations, 'primary'),
-            ('open', 'Open cells', self.open_cells, 'primary'),
-            ('delete', 'Delete last cell', self.delete_last_cell, 'warning'),
-            ('run', 'Run cells', self.run_cells, 'primary'),
-            ('add', 'Add limitation', self.add_limitation, 'primary'),
-            ('remove', 'Remove limitation', self.remove_limitation, 'warning'),
-            ('start', 'Start analysis', self.start_analysis_clicked, 'success'),
-            ('assist', 'AI assistant', self.toggle_ai, 'success'),
-            ('limitations', 'Add limitations', self.display_limitations_view, 'success')
+            ('save', 'Save description', self._start_data_import, 'success'),
+            ('analyze', 'Analyze this data', self._import_data, 'success'),
+            ('test', 'Test', self._check_variable_independence, 'success'),
+            ('close', 'Close test', self._close_independence_test, 'warning'),
+            ('independence', 'Test independence', self._display_independence_test, 'success'),
+            ('import', 'Import manually', self._show_cell_operations, 'primary'),
+            ('open', 'Open cells', self._open_cells, 'primary'),
+            ('delete', 'Delete last cell', self._delete_last_cell, 'warning'),
+            ('run', 'Run cells', self._run_cells, 'primary'),
+            ('add', 'Add limitation', self._add_limitation, 'primary'),
+            ('remove', 'Remove limitations', self._remove_limitation, 'warning'),
+            ('start', 'Start analysis', self._start_analysis_clicked, 'success'),
+            ('assist', 'AI assistant', self._toggle_ai, 'success'),
+            ('limitations', 'Check limitations', self._display_limitations_view, 'success'),
+            ('choose', 'Choose data file', self._display_file_chooser, 'warning')
         ]
+
         return button_list
 
-    def toggle_ai(self, _=None):
+    def _toggle_ai(self, _=None):
         """Button function to open/close AI assistant"""
 
         if self.buttons['assist'].description == 'AI assistant':
@@ -60,13 +75,24 @@ class Bodi:
             self.buttons['assist'].button_style = 'success'
         self.next_step[0] = 'toggle_ai'
 
+    def _add_limitation(self, _=None):
+        """Button function for adding new limitation."""
+
+        self.limitations.add_limitation()
+        self._display_limitations_view()
+
+    def _remove_limitation(self, _=None):
+        """Button function for removing limitation."""
+
+        self.limitations.remove_limitations()
+        self._display_limitations_view()
+
     def data_preparation_grid(self, message=None):
         """Creates widget grid.
-        
+
         Args:
             message (widget, optional): HTML widget to be displayed
         """
-
         self.limitations.set_error_value('')
         cell_number_label = self.bogui.create_label(
             'Add code cells for data preparation:')
@@ -88,7 +114,7 @@ class Bodi:
         grid = widgets.AppLayout(
             left_sidebar=widgets.HBox([
                 cell_number_label,
-                self.add_cells_int
+                self.fields[3]
             ]),
             right_sidebar=buttons,
             footer=message,
@@ -96,46 +122,45 @@ class Bodi:
             pane_heights=['0px', '80px', 1],
             grid_gap='12px'
         )
-
         return grid
 
-    def show_cell_operations(self, _=None):
+    def _show_cell_operations(self, _=None):
         """Button function to show buttons for cell operations."""
 
-        self.buttons['independence'].disabled = True
         clear_output(wait=True)
+        display(self.buttons['choose'])
         display(self.data_preparation_grid())
 
-    def open_cells(self, _=None):
+    def _open_cells(self, _=None):
         """Button function that opens selected number of cells above widget cell"""
-        if self.add_cells_int.value > 0:
-            self.cell_count += self.add_cells_int.value
-            self.boutils.create_code_cells_above(self.add_cells_int.value)
 
-    def delete_last_cell(self, _=None):
+        self.buttons['choose'].disabled = True
+
+        if self.fields[3].value > 0:
+            self.cell_count += self.fields[3].value
+            self.boutils.create_code_cells_above(self.fields[3].value)
+
+    def _delete_last_cell(self, _=None):
         """Button function to delete the last data import code cell"""
 
         if self.cell_count > 0:
             self.boutils.delete_cell_above()
             self.cell_count -= 1
+        if self.cell_count == 0:
+            self.buttons['choose'].disabled = False
 
-    def run_cells(self, _=None):
+    def _run_cells(self, _=None):
         """Button function that runs data import cells."""
 
         clear_output(wait=True)
         display(self.data_preparation_grid(
-            message=self.limitations.get_limitations_as_bullet_list()))
+            message=self.limitations.get_limitations_for_print()))
         not_normal = self.check_normal_distribution(self.stattests.dataset)
         self.boutils.check_cells_above(self.cell_count, 'ttest', not_normal)
         self.boutils.run_cells_above(self.cell_count)
 
-        # self.stattests.detect_tests()
-        # self.display_limitations_view()
-
-    def display_limitations_view(self, _=None):
+    def _display_limitations_view(self, _=None):
         """Displays limitation view."""
-
-        # self.buttons['independence'].disabled = True
 
         limitation_grid = self.limitations.create_limitation_grid()
         limitation_grid.footer=widgets.VBox([
@@ -145,23 +170,10 @@ class Bodi:
                 self.buttons['remove']
             ])
         ])
-
-
-
-        self.show_cell_operations()
+        clear_output(wait=True)
+        display(self.data_preparation_grid())
         display(limitation_grid)
         display(self.buttons['start'])
-
-    def add_limitation(self, _=None):
-        """Button function for adding new limitation."""
-
-        self.limitations.add_limitation()
-        self.display_limitations_view()
-
-    def remove_limitation(self, _=None):
-        """Button function for removing limitation."""
-        self.limitations.remove_limitation()
-        self.display_limitations_view()
 
     def format_data_description(self):
         """Formats data description for markdown
@@ -169,13 +181,13 @@ class Bodi:
         Returns:
             formatted_text (str)
         """
-        title = f'# {self.title.value}'
-        dataset = f'{self.data_name.value}'
-        description = '<br />'.join(self.data_description.value.split('\n'))
+        title = f'# {self.fields[0].value}'
+        dataset = f'{self.fields[1].value}'
+        description = '<br />'.join(self.fields[2].value.split('\n'))
         formatted_text = f'{title}\\n ## Data: {dataset}\\n ### Description\\n{description}'
         return formatted_text
 
-    def start_analysis_clicked(self, _=None):
+    def _start_analysis_clicked(self, _=None):
         """Button function to start analysis after data preparation"""
         if self.limitations.call_check_limitation():
             text = self.limitations.format_limitations()
@@ -186,60 +198,62 @@ class Bodi:
             self.limitations.set_error_value('Data limitations cannot be empty')
 
     def fc_callback(self):
-        """Opens two code cells to import pandas and read a csv file
-        after the user has selected the file."""
+        """Shows buttons to continue with selected data file or import manually."""
+
+        clear_output(wait=True)
 
         if self.file_chooser.selected.endswith('.csv'):
-            self.file_chooser.title = self.file_chooser.selected_filename
-            self.boutils.create_code_cells_above(2)
-            self.boutils.execute_cell_from_current(
-                distance=-2,
-                code='import pandas as pd',
-                hide_input=False
-            )
-            self.boutils.execute_cell_from_current(
-                distance=1,
-                code=f"data_frame = pd.read_csv('{self.file_chooser.selected}')",
-                hide_input=False
-            )
-            self.file_chooser.register_callback(self.fc_callback_on_change)
-            clear_output(wait=True)
+            self.file_chooser.title = f'Selected file: {self.file_chooser.selected_filename}'
             display(widgets.VBox([
                 self.file_chooser,
                 self.buttons['analyze']
             ]))
-
         else:
-            self.file_chooser.title = 'Unknown file type, choose a csv file or import manually.'
-            clear_output(wait=True)
+            self.file_chooser.title = 'Unknown file type: choose a csv file or import manually.'
             display(widgets.VBox([
                 self.file_chooser,
                 self.buttons['import']
             ]))
 
-    def fc_callback_on_change(self):
-        """Reads another csv file without importing pandas."""
+    def _import_data(self, _=None):
+        """Imports selected data in code cells and opens next view for data preparation."""
 
-        if self.file_chooser.selected.endswith('.csv'):
-            self.file_chooser.title = self.file_chooser.selected_filename
-            self.boutils.execute_cell_from_current(
-                distance=-1,
-                code=f"data_frame = pd.read_csv('{self.file_chooser.selected}')",
-                hide_input=False
-            )
-            clear_output(wait=True)
-            display(widgets.VBox([
-                self.file_chooser,
-                self.buttons['analyze']
-            ]))
+        self.boutils.create_code_cells_above(2)
+        self.boutils.execute_cell_from_current(
+            distance=-2,
+            code='import pandas as pd',
+            hide_input=False
+        )
+        self.boutils.execute_cell_from_current(
+            distance=1,
+            code=f"data_frame = pd.read_csv('{self.file_chooser.selected}')",
+            hide_input=False
+        )
+        # Load config files for the tests to be checkked
+        #checklist = self.load_cfg_file(self.file_chooser.selected_path)
+        self.check_variables()
 
-        else:
-            self.file_chooser.title = 'Unknown file type, choose a csv file or import manually.'
-            clear_output(wait=True)
-            display(widgets.VBox([
-                self.file_chooser,
-                self.buttons['import']
-            ]))
+    def load_cfg_file(self, cfg_path):
+        """Checks if given path contains file bringorder.cfg. If found,
+        returns a list with the tests specified in the config file. Otherwise
+        returns the list containing 'ttest'.
+
+        args:
+            cfg_path: directory containing bringorder.cfg_file
+        returns:
+            tests_to_check: list of test names
+        """
+        cfg_file = cfg_path + "/bringorder.cfg"
+        tests_to_check = []
+        try:
+            with open(cfg_file) as get_cfg:
+                for line in get_cfg:
+                    line = line.replace("\n", "")
+                    tests_to_check.append(line)
+        except FileNotFoundError:
+            tests_to_check.append('ttest')
+        return tests_to_check
+
 
     def check_normal_distribution(self, data_frame):
         """Checks which variables are not normally distributed.
@@ -250,7 +264,6 @@ class Bodi:
         Returns:
             not_normal_dist (list): List of variables that are not normally distributed
         """
-
         n_distributed = self.stattests.check_numerical_data(data_frame)
         not_normal_dist = []
 
@@ -260,10 +273,10 @@ class Bodi:
 
         return not_normal_dist
 
-    def check_variable_independence(self, _=None):
+    def _check_variable_independence(self, _=None):
         """Button function for independence test. Checks independence and adds limitation
-        if variables are not independent."""
-
+        if variables are not independent.
+        """
         result = self.stattests.check_variable_independence()
 
         if isinstance(result[2], str):
@@ -275,15 +288,13 @@ class Bodi:
                 if self.limitations.data_limitations[-1].value != '':
                     self.limitations.add_limitation()
                 self.limitations.data_limitations[-1].value = limitation
-
-            message = self.bogui.create_message(limitation)
-
+            message = self.bogui.create_message(f'Result added to limitations: {limitation}')
         else:
             message = self.bogui.create_message(f'{result[0]} and {result[1]} are independent')
 
         display(message)
 
-    def display_independence_test(self, _=None):
+    def _display_independence_test(self, _=None):
         """Displays independence test and disables other buttons."""
 
         independence_test = self.stattests.select_variables()
@@ -293,7 +304,7 @@ class Bodi:
                 self.buttons['close']
             ])
 
-            for button in ['open', 'delete', 'run', 'assist']:
+            for button in ['open', 'delete', 'run', 'assist', 'limitations']:
                 self.buttons[button].disabled = True
 
             clear_output(wait=True)
@@ -302,24 +313,20 @@ class Bodi:
         self.buttons['independence'].disabled = True
         display(independence_test)
 
-    def close_independence_test(self, _=None):
+    def _close_independence_test(self, _=None):
         """Closes the independence test and activates buttons."""
 
-        for button in ['open', 'delete', 'run', 'independence', 'assist']:
+        for button in ['open', 'delete', 'run', 'independence', 'assist', 'limitations']:
             self.buttons[button].disabled = False
 
         clear_output(wait=True)
-        if self.limitations.data_limitations[-1].value != '':
-            message = self.limitations.get_limitations_as_bullet_list()
-            display(self.data_preparation_grid(message=message))
+        message = self.limitations.get_limitations_for_print()
+        display(self.data_preparation_grid(message=message))
 
-        else:
-            display(self.data_preparation_grid())
-
-    def check_variables(self, _=None):
-        """Button function to check if data variables are normally distributed.
-        Displays buttons for independence test and cell operations."""
-
+    def check_variables(self):
+        """Checks if data variables are normally distributed.
+        Displays buttons for independence test and cell operations.
+        """
         data_frame = pd.read_csv(self.file_chooser.selected)
         self.stattests.dataset = data_frame
 
@@ -332,35 +339,40 @@ class Bodi:
 
         clear_output(wait=True)
         if len(not_normal) > 0:
-            message = self.limitations.get_limitations_as_bullet_list()
+            message = self.limitations.get_limitations_for_print()
             display(self.data_preparation_grid(message=message))
-
         else:
             display(self.data_preparation_grid())
 
-    def start_data_import(self, _=None):
-        """Creates markdown for data description and shows buttons for data import"""
-        if self.title.value == '':
-            self.bodi(error = 'Please give your study a title')
-        elif self.data_name.value == '':
-            self.bodi(error = 'You must name the data set')
-        elif self.data_description.value == '':
-            self.bodi(error = 'You must give some description of the data')
+    def _display_file_chooser(self, _=None):
+        """Displays file chooser for data import."""
 
+        clear_output(wait=True)
+        display(widgets.VBox([
+            self.file_chooser,
+            self.buttons['import']
+        ]))
+
+    def _start_data_import(self, _=None):
+        """Creates markdown for data description and shows buttons for data import"""
+
+        if self.fields[0].value == '':
+            self.bodi(error = 'Please give your study a title')
+        elif self.fields[1].value == '':
+            self.bodi(error = 'You must name the data set')
+        elif self.fields[2].value == '':
+            self.bodi(error = 'You must give some description of the data')
         else:
             self.boutils.create_markdown_cells_above(1, text=self.format_data_description())
-
             self.file_chooser.register_callback(self.fc_callback)
             self.file_chooser.title = 'Choose a data file:'
-
-            clear_output(wait=True)
-            display(widgets.VBox([
-                self.file_chooser,
-                self.buttons['import']
-            ]))
+            self._display_file_chooser()
 
     def bodi(self, error=''):
-        """Main function"""
+        """Starts data import phase by asking titles and data description.
+
+        args: Error message: str
+        """
         self.boutils.hide_current_input()
         self.boutils.hide_selected_input()
 
@@ -378,9 +390,9 @@ class Bodi:
                 description_label
             ]),
             center=widgets.VBox([
-                    self.title,
-                    self.data_name,
-                    self.data_description
+                    self.fields[0],
+                    self.fields[1],
+                    self.fields[2]
             ]),
             footer = widgets.HBox([
                 self.buttons['save'],
@@ -394,8 +406,8 @@ class Bodi:
         display(grid)
 
         if 'name' in error:
-            self.data_name.focus()
+            self.fields[1].focus()
         elif 'description' in error:
-            self.data_description.focus()
+            self.fields[2].focus()
         else:
-            self.title.focus()
+            self.fields[0].focus()
