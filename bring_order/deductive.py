93e130ed387c0c6a77656c1aef5cdcb8d0c5df18
Tino Pönni <tino.ponni@tutanota.com>
2023-08-19 13:02:22 +0300

    Removed unnessessary nlp-model

--- bring_order/deductive.py
+++ bring_order/deductive.py
@@ -33,10 +33,6 @@ class Deductive:
         self.data_limitations = [self.bogui.create_input_field('Data limitations missing')]
         self.result_description = self.bogui.create_text_area('', 'Results')
 
-        class_dir = os.path.dirname(os.path.realpath(__file__))
-        model_path="language_models/en_core_web_sm/en_core_web_sm-3.6.0"
-        self.nlp = spacy.load(os.path.join(class_dir, model_path))
-
     @property
     def button_list(self):
         """Buttons for deductive class.
